a36fca54509de149a2fcba37e1551da291248237
Gavrisciuc Vladislav <angelbeats9632@gmail.com>
2018-03-08 19:36:02 +0200

    OBJ to JSON and inverse

--- src/main/java/com/apchimeow/lab/JSONtoOBJ/SerializeToXml.java
+++ src/main/java/com/apchimeow/lab/JSONtoOBJ/SerializeToXml.java
@@ -3,26 +3,26 @@ package com.apchimeow.lab.JSONtoOBJ;
 
 import java.io.*;
 import java.util.Date;
-import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class SerializeToXml {
     public static void main(String[] args) throws IOException {
-        XmlMapper xmlMapper = new XmlMapper();
+        ObjectMapper mapper = new ObjectMapper();
 
         String[] inventory = {"Броня", "Оружие", "Монеты"};
         CharClass profa = new CharClass(CharClass.profa.MAG, 100, 100);
 
-        xmlMapper.writeValue(new File("src/main/resources/myCharacterList1.xml"), new Character("myNick", profa, 5, inventory, new Date(2018-1900, 3-1, 8)));
-        File file = new File("src/main/resources/myCharacterList1.xml");
+        mapper.writeValue(new File("src/main/resources/myCharacterList1.json"), new Character("myNick", profa, 5, inventory, new Date(2018-1900, 3-1, 8)));
+        File file = new File("src/main/resources/myCharacterList1.json");
 
         fromXmlFile();
     }
 
     public static void fromXmlFile() throws IOException {
-        File file = new File("src/main/resources/myCharacterList1.xml");
-        XmlMapper xmlMapper = new XmlMapper();
-        String xml = inputStreamToString(new FileInputStream(file));
-        Character value = xmlMapper.readValue(xml, Character.class);
+        File file = new File("src/main/resources/myCharacterList1.json");
+        ObjectMapper mapper = new ObjectMapper();
+        String str = inputStreamToString(new FileInputStream(file));
+        Character value = mapper.readValue(str, Character.class);
         System.out.println(value);
     }
 
